261964188b7298da283ae8165f920efcaac581c8
mohdshahil-akacorptech <shahil@akacorptech.com>
2025-10-08 13:46:29 +0530

    Added the message whenever a new user joined.

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import { Server } from 'socket.io';
 import { createServer } from 'http';
+import { type } from 'os';
+import { text } from 'stream/consumers';
 
 const app = express();
 const server = createServer(app);
 const PORT = 3000;
 const io = new Server(server, {
-    connectionStateRecovery: {}
+    connectionStateRecovery: true,
 });
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -19,20 +21,42 @@ app.get('/', (req, res) => {
 
 
 io.on('connection', (socket) => {
+
+    if(socket.recovered) {
+        console.log("User reconnected")
+        return;
+    }
+
     console.log('a user is connected');
 
-    socket.on('disconnect', () => {
-        console.log('a user is disconnected')
-    })
-
-
+    socket.username="Anonymous";
     
-    socket.on('chat message', (msg) => {
-        console.log('message : '+ msg)
-        io.emit('chat message', msg)
-    })
-})
+    // when a new user joins
+    socket.emit('chat message', { text: "Welcome to the chat!", type: 'system'});
 
+    // notifying others that a user just join the chat!
+    socket.broadcast.emit('chat message', { text: "A new user just joined the chat!", type: 'system' });
+
+    socket.on('chat message', (msg) => {
+        console.log('message : '+ msg.text)
+        io.emit('chat message', {
+            username:socket.username,
+            text: msg.text, 
+            type: 'user' })
+    })
+
+    socket.on('disconnect', (reason) =>  {
+
+        if(reason === "io server disconnect" || reason === "ping timeout") {
+            console.log("Temporary disconnection, skipping 'user left'");
+            return;
+        }
+
+        console.log('a user is disconnected')
+        io.emit('chat message',{ text: "A user left the chat!", type: 'system'});
+    })
+    
+})
 
 server.listen(PORT, () => {
     console.log(`Server is listening on PORT ${PORT}`);
